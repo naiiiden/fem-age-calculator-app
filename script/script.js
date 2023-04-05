3436d3475321b973d0aa3107a13b70efe3c8793b
naiiiden <na4ko359@gmail.com>
2023-04-05 17:13:18 +0300

    add date validation & improve calculation as suggested by chatgpt

--- script/script.js
+++ script/script.js
@@ -1,7 +1,6 @@
 document.querySelector(".form-submit-button").addEventListener("click", (e) => {
     e.preventDefault();
 
-    // Get the user's input
     const dayOfBirth = parseInt(document.getElementById("day-input").value);
     const monthOfBirth = parseInt(document.getElementById("month-input").value);
     const yearOfBirth = parseInt(document.getElementById("year-input").value);
@@ -10,22 +9,37 @@ document.querySelector(".form-submit-button").addEventListener("click", (e) => {
     const maxDaysInMonth = new Date(yearOfBirth, monthOfBirth, 0).getDate();
     if (isNaN(dayOfBirth) || dayOfBirth < 1 || dayOfBirth > maxDaysInMonth || isNaN(monthOfBirth) || monthOfBirth < 1 || monthOfBirth > 12 || isNaN(yearOfBirth)) {
         console.error("Invalid date entered");
+        document.querySelector(".day-error").textContent = "Must be a valid date";
+        document.querySelectorAll("input").forEach(input => {
+            input.style.borderColor = "#ff5757";
+        });
         return;
+    } else {
+        document.querySelector(".day-error").textContent = "";
+        document.querySelectorAll("input").forEach(input => {
+            input.style.borderColor = "#dbdbdb";
+        });
     }
 
     // Create a Date object for the user's birthdate
     const birthdate = new Date(yearOfBirth, monthOfBirth - 1, dayOfBirth);
-
-    // Get the current date
     const currentDate = new Date();
 
-    // Calculate the difference between the two dates in milliseconds
-    const differenceInMilliseconds = currentDate.getTime() - birthdate.getTime();
-
     // Convert the difference to years, months, and days
-    const differenceInYears = Math.floor(differenceInMilliseconds / (1000 * 60 * 60 * 24 * 365.25));
-    const differenceInMonths = Math.floor((differenceInMilliseconds / (1000 * 60 * 60 * 24 * 30.44)) % 12);
-    const differenceInDays = Math.floor((differenceInMilliseconds / (1000 * 60 * 60 * 24)) % 30.44);
+    let differenceInYears = currentDate.getFullYear() - birthdate.getFullYear();
+    let differenceInMonths = currentDate.getMonth() - birthdate.getMonth();
+    let differenceInDays = currentDate.getDate() - birthdate.getDate();
+
+    // Adjust years, months, and days if needed
+    if (differenceInMonths < 0 || (differenceInMonths === 0 && differenceInDays < 0)) {
+        differenceInYears--;
+        differenceInMonths += 12;
+        if (differenceInDays < 0) {
+            const maxDaysInLastMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
+            differenceInDays += maxDaysInLastMonth;
+            differenceInMonths--;
+        }
+    }
 
     document.querySelector(".years-number").textContent = differenceInYears;
     document.querySelector(".months-number").textContent = differenceInMonths;
